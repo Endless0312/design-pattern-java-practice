60aa5076f12772c6069d51fc5f581f60aa48be4f
YuChunYun <littleyuantw@gmail.com>
2018-05-02 16:27:52 +0800

    Add [Singleton] Pattern Test Case

---
+++ src/test/java/idv/ycy/practice/designpattern/singleton/PatternTest.java
@@ -0,0 +1,29 @@
+package idv.ycy.practice.designpattern.singleton;
+
+import idv.ycy.designpattern.creational.singleton.Car;
+import junit.framework.TestCase;
+
+public class PatternTest extends TestCase {
+
+	Car car = Car.getInstance();
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testPattern() {
+		
+		Car firstCar = car.getInstance();
+		Car secondCar = car.getInstance();
+		
+		int firstCarHash = System.identityHashCode(firstCar);
+		int secondCarHash = System.identityHashCode(secondCar);
+		
+		assertEquals(firstCarHash, secondCarHash);
+		
+	}
+}
